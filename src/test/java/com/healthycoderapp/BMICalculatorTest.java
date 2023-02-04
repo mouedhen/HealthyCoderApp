3008bec48bfe857c6d16036feb096e243cf29759
Mohamed Chams Eddin Mouedhen <mohamed.mouedhen@mass-analytics.com>
2023-02-04 19:44:58 +0100

    First commit

---
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,14 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    @Test
+    void test() {
+        assertTrue(BMICalculator.isDietRecommended(81.2, 1.65));
+    }
+
+}
