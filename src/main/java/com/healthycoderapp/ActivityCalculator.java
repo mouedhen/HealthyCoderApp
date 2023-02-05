76dd11a6d949376341ac5989761dbe0f785e9579
Mohamed Chams Eddin Mouedhen <mohamed.mouedhen@mass-analytics.com>
2023-02-05 13:56:26 +0100

    TDD Approach - Implement method rateActivityLevel to pass test cases

--- src/main/java/com/healthycoderapp/ActivityCalculator.java
+++ src/main/java/com/healthycoderapp/ActivityCalculator.java
@@ -4,6 +4,21 @@ public class ActivityCalculator {
     private static final int WORKOUT_DURATION_MIN = 45;
 
     public static String rateActivityLevel(int weeklyCardioMin, int weeklyWorkoutSessions) {
-        return "";
+        if (weeklyCardioMin < 0 || weeklyWorkoutSessions < 0) {
+            throw new RuntimeException("Input below 0");
+        }
+
+        int totalMinutes = weeklyCardioMin + weeklyWorkoutSessions * WORKOUT_DURATION_MIN;
+        double avgDailyActivityMin = totalMinutes / 7.0;
+
+        if (avgDailyActivityMin < 20) {
+            return "bad";
+        }
+
+        if (avgDailyActivityMin < 40) {
+            return "average";
+        }
+
+        return "good";
     }
 }
